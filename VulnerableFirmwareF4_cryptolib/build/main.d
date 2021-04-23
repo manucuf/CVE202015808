d5cdc084161d9b86d8bb79890e25c8da683536fa
Emanuele Cufino <emanuele.cufino@outlook.com>
2021-04-23 13:30:03 +0200

    add exploit for cryptolib

---
+++ VulnerableFirmwareF4_cryptolib/build/main.d
@@ -0,0 +1,346 @@
+build/main.o: Core/Src/main.c Core/Inc/main.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ Core/Inc/stm32f4xx_hal_conf.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ USB_DEVICE/App/usb_device.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ USB_DEVICE/Target/usbd_conf.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/crypto.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/config.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/types.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/macros.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/err_codes.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/config.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/types.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/err_codes.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common/sk.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/aes.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/Common/aes_low_level.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/Common/aes_common.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/ECB/aes_ecb.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/CBC/aes_cbc.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/CTR/aes_ctr.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/CFB/aes_cfb.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/OFB/aes_ofb.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/XTS/aes_xts.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/GCM/aes_gcm.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/CCM/aes_ccm.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/CMAC/aes_cmac.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/AES/KEYWRAP/aes_keywrap.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/DES/des.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/DES/Common/des_common.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/DES/ECB/des_ecb.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/DES/CBC/des_cbc.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/TDES/tdes.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/TDES/Common/tdes_common.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/TDES/ECB/tdes_ecb.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/TDES/CBC/tdes_cbc.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ARC4/arc4.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/hash.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/hash_common.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/MD5/md5.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/MD5/hmac_md5.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA1/sha1.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA1/hmac_sha1.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA224/sha224.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/sha256_sha224_transform.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA224/hmac_sha224.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA256/sha256.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA256/hmac_sha256.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA384/sha384.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/sha512_sha384_transform.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA384/hmac_sha384.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/sha512.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hmac_sha512.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hkdf512.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hmac_sha512.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/math.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/bn.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/conv.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/bn.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/arith.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/monty.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/modular.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/rng_bn.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/../../../RNG/rng.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/../../../RNG/DRBG_AES128/drbg.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/RSA/rsa.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/RSA/PKCS\#1v15/rsa_pkcs1v15.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/RSA/LowLevel/rsa_low_level.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/RNG/rng.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/ecc.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/math.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/bn.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/conv.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/arith.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/monty.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/modular.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/rng_bn.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/POLY1305/poly1305.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/ED25519/ed25519.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/C25519/c25519.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/CHACHA/chacha.h \
+ Middlewares/ST/STM32_Cryptographic/Inc/CHACHA20-POLY1305/chacha20-poly1305.h \
+ USB_DEVICE/App/usbd_cdc_if.h \
+ Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+
+Core/Inc/main.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+Core/Inc/stm32f4xx_hal_conf.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+Drivers/CMSIS/Include/cmsis_version.h:
+
+Drivers/CMSIS/Include/cmsis_compiler.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/mpu_armv7.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+USB_DEVICE/App/usb_device.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+USB_DEVICE/Target/usbd_conf.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/crypto.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/config.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/types.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/macros.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/err_codes.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/config.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/types.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/err_codes.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common/sk.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/aes.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/Common/aes_low_level.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/Common/aes_common.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/ECB/aes_ecb.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/CBC/aes_cbc.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/CTR/aes_ctr.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/CFB/aes_cfb.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/OFB/aes_ofb.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/XTS/aes_xts.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/GCM/aes_gcm.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/CCM/aes_ccm.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/CMAC/aes_cmac.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/AES/KEYWRAP/aes_keywrap.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/DES/des.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/DES/Common/des_common.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/DES/ECB/des_ecb.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/DES/CBC/des_cbc.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/TDES/tdes.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/TDES/Common/tdes_common.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/TDES/ECB/tdes_ecb.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/TDES/CBC/tdes_cbc.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ARC4/arc4.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/hash.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/hash_common.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/MD5/md5.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/MD5/hmac_md5.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA1/sha1.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA1/hmac_sha1.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA224/sha224.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/sha256_sha224_transform.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA224/hmac_sha224.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA256/sha256.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA256/hmac_sha256.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA384/sha384.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/Common/sha512_sha384_transform.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA384/hmac_sha384.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/sha512.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hmac_sha512.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hkdf512.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/HASH/SHA512/hmac_sha512.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/math.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/bn.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/conv.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/bn.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/arith.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/monty.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/modular.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/rng_bn.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/../../../RNG/rng.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/Common_ecc_rsa/MATH/BN/../../../RNG/DRBG_AES128/drbg.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/RSA/rsa.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/RSA/PKCS\#1v15/rsa_pkcs1v15.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/RSA/LowLevel/rsa_low_level.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/RNG/rng.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/ecc.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/math.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/bn.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/conv.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/arith.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/monty.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/modular.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ECC/../Common_ecc_rsa/MATH/BN/rng_bn.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/POLY1305/poly1305.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/ED25519/ed25519.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/C25519/c25519.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/CHACHA/chacha.h:
+
+Middlewares/ST/STM32_Cryptographic/Inc/CHACHA20-POLY1305/chacha20-poly1305.h:
+
+USB_DEVICE/App/usbd_cdc_if.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
